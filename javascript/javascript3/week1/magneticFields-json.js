a1d681efdda6597120d96f5ea6fb392da6e35e1c
bengü thon mai mochi <benguozakinci@gmail.com>
2020-01-13 15:21:33 +0000

    Add files via upload

---
+++ javascript/javascript3/week1/magneticFields-json.js
@@ -0,0 +1,77 @@
+[
+    {
+        "name": "Distant Plastic Trees",
+        "year": 1992,
+        "type" : "album",
+        "songs" : [
+                    "Railroad Boy", 
+                    "Smoke Signals",
+                    "You Love To Fail",
+                    "Kings", 
+                    "Babies Failing", 
+                    "Living In An Abandoned Firehouse With You",
+                    "Tar-Heel Boy",
+                    "Falling In Love With The Wolfboy", 
+                    "Josephine", 
+                    "100,000 Fireflies", 
+                    "Plant White Roses" 
+                ]
+    },
+    {
+        "name": "100,000 Fireflies",
+        "year": 1991,
+        "type" : "single",
+        "songs" : [
+            "100,000 Fireflies", 
+            "Old Orchard Beach"
+        ]
+    },
+    {
+        "name": "The Wayward Bus",
+        "year": 1992,
+        "type": "album",
+        "songs" : [
+            "When You Were My Baby", 
+            "The Saddest Story Ever Told",
+            "Lovers From The Moon",
+            "Kings", 
+            "Candy", 
+            "Tokyo Á Go-Go",
+            "Summer Lies",
+            "Old Orchard Beach", 
+            "Jeremy", 
+            "Dancing In Your Eyes", 
+            "Suddenly There Is A Tidal Wave",
+            "Railroad Boy", 
+            "Smoke Signals", 
+            "Kings", 
+            "Babies Falling",
+            "Living In An Abandoned Firehouse With You",
+            "Tar-Heel Boy",
+            "Falling In Love With The Wolfboy",
+            "Josephine",
+            "100,000 Fireflies"
+        ]
+    },
+    {
+        "name": "Holiday",
+        "year": 1994,
+        "type": "album",
+        "songs" : [
+            "BBC Radiophonic Workshop", 
+            "Desert Island",
+            "Deep Sea Diving Suit",
+            "Strange Powers", 
+            "Torn Green Velvet Eyes", 
+            "The Flowers She Sent And The Flowers She Said She Sent",
+            "Swinging London",
+            "In My Secret Place", 
+            "Sad Little Moon", 
+            "The Trouble I've Been Looking For", 
+            "Sugar World",
+            "All You Ever Do Is Walk Away", 
+            "In My Car", 
+            "Take Ecstasy With Me"
+        ]
+    }
+]
