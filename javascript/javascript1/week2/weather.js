bf021d810fd64b2f56b4506a984adf4a7deeb079
bengü thon mai mochi <benguozakinci@gmail.com>
2020-02-05 19:50:48 +0000

    Refactor

--- javascript/javascript1/week2/weather.js
+++ javascript/javascript1/week2/weather.js
@@ -1,22 +1,17 @@
-const clothesToWear = function wearMe(temperature) {
-  if (temperature > 30) {
+const whichClothesToWear = function wearMe(temperature) {
+  if (temperature === "") {
+    return "What's the temperature?";
+  } else if (temperature > 30) {
     return "You should take your swimming suits with you";
-  }
-  else if (temperature <= 30 &&  temperature >= 20) {
+  } else if (temperature <= 30 && temperature >= 20) {
     return "Shorts and tshirts!";
-  }
-  else if (temperature < 20 && temperature > 10) {
+  } else if (temperature < 20 && temperature > 10) {
     return "Jeans and tshirts";
-  }
-  else if (temperature <= 10 && temperature > 0) {
+  } else if (temperature <= 10 && temperature > 0) {
     return "Winter coat";
+  } else {
+    return "Extreme weather conditions, we have no clue what to wear!";
   }
-  else if (temperature <= 0) {
-    return "Arctic clothes";
-  }
-  else {
-    return "What's the temperature? ";
-  }
-}
+};
 
-console.log(clothesToWear(33));
+console.log(whichClothesToWear(33));
