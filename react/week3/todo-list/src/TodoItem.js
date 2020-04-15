33e4df77bde4bdfe82b819e54964e76c0bf0d926
mochi <benguozakinci@gmail.com>
2020-04-15 17:10:54 +0000

    Initial Commit with fetch API & edit button & add form w/  datepicker

---
+++ react/week3/todo-list/src/TodoItem.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TodoItem.css';
+
+function TodoItem(props){
+    const {description, id, deadline, deleteTodo, todoItemClicked, setIsEditing, isEditing } = props;
+    const { isChecked } = props;
+    const { rewriteDescription } = props;
+
+    const todoItemChecked = event => {
+        todoItemClicked(id, event.target.checked);
+    }
+
+    const changeEditingStatus = () => {
+        setIsEditing(id);
+    }
+
+    const updateDescription = event => {
+        rewriteDescription(id, event)
+    }
+
+    return(  
+        <>
+        {description === undefined ? (
+            alert('Your todo description is empty')
+        ) : (
+            isEditing ? ( 
+                <li className='todo-item-container'>
+                    <input type="text" onChange={updateDescription} value={description} />  
+                    <input type="button" className="list-item-button" value="Update" onClick={changeEditingStatus} /> 
+                    <button className="list-item-button" onClick={() => deleteTodo(id)}>Delete!</button>
+                    <label>Check</label>
+                    <input type="checkbox" id="check" checked={isChecked} onChange={todoItemChecked}/>
+                </li>
+                ) : (
+                <li className='todo-item-container'>
+                    <h4 className={isChecked ? 'checked' : ''} > {description} / {deadline.toString()} </h4>   
+                    <input type="button" className="list-item-button" value="Edit" onClick={changeEditingStatus} />
+                    <button className="list-item-button" onClick={() => deleteTodo(id)}>Delete!</button>   
+                    <label>Check</label>
+                    <input type="checkbox" id="check" checked={isChecked} onChange={todoItemChecked}/>
+                </li>
+                )
+            )
+        }
+        </>      
+    )
+}
+
+export default TodoItem;
+
+TodoItem.propTypes = {
+    isEditing: PropTypes.bool,
+    description: PropTypes.string.isRequired,
+    updateDescription: PropTypes.func,
+    changeEditingStatus: PropTypes.func,
+    isChecked: PropTypes.bool,
+    todoItemChecked: PropTypes.func,
+    id:  PropTypes.oneOfType([
+            PropTypes.instanceOf(Date),
+            PropTypes.number
+    ]),
+    deadline: PropTypes.oneOfType([
+            PropTypes.instanceOf(Date),
+            PropTypes.string
+    ])
+};
