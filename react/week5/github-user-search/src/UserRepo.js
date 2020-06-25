9f90e00210df145eb4be98e77448da40a95564be
mochi <benguozakinci@gmail.com>
2020-06-25 12:28:44 +0000

    Update functionality

--- react/week5/github-user-search/src/UserRepo.js
+++ react/week5/github-user-search/src/UserRepo.js
@@ -17,22 +17,11 @@ const UserRepo = ( {username} ) => {
         fetchData();
     }, [username]);
 
-    return(
-            <>
-                {userRepos.length > 0 
-                    ? userRepos.map(repo =>
-                        <li key={repo.name}>
-                            <h4>{repo.name}</h4>
-                            Language: {repo.language}
-                            Description: {repo.language}
-                            <a href={repo.html_url}>Visit for details!</a>
-                        </li>
-                    )
-                    :
-                    <p>The user doesn't have a public repo.</p> 
-                }
-            </>
-    )
+    return (
+        <div>
+            {console.log(userRepos)}
+        </div>
+    );
 };
 
 export default UserRepo;
